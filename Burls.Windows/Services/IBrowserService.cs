b77da3f8b090cbf442e723bf69ca3aca6355f902
Yves-Laurent Creton <52154171+BEzGumpTion@users.noreply.github.com>
2020-06-24 12:43:32 +0200

    Merged Feature/issue 3 to maser (#9)
    * Added the viewing of the browsers in Windows
    
    * Added gitignore, removed everything
    
    * Readded the projects

---
+++ Burls.Windows/Services/IBrowserService.cs
@@ -0,0 +1,10 @@
+﻿using Burls.Windows.Models;
+using System.Collections.Generic;
+
+namespace Burls.Windows.Services
+{
+    public interface IBrowserService
+    {
+        IReadOnlyList<Browser> GetBrowsers();
+    }
+}
