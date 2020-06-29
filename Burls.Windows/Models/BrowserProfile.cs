0a1071eb0f4003b2a54cbedb23ce3fa44bc5ca63
Yves-Laurent Creton <yveslaurent.creton@gmail.com>
2020-06-29 17:53:47 +0200

    Change the way browsers and profiles are displayed to one list

---
+++ Burls.Windows/Models/BrowserProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burls.Windows.Models
+{
+    public class BrowserProfile
+    {
+        public string Name { get; }
+        public Browser Browser { get; }
+        public Profile Profile { get; }
+
+        public BrowserProfile(Browser browser, Profile profile)
+        {
+            Name = $"{browser.Name} - {profile.Name}";
+            Browser = browser;
+            Profile = profile;
+        }
+
+        public void NavigateToUrl(string url)
+        {
+            Browser.NavigateToUrl(url, Profile);
+        }
+    }
+}
