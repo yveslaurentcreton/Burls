fc68fb2d664021552e807e324f6f7e1355c8bae6
Yves-Laurent Creton <yveslaurent.creton@gmail.com>
2021-12-16 07:15:02 +0100

    Added center screen + changed the width and height on startup + added dependencies to wix

--- Burls.Windows/MainWindow.xaml.cs
+++ Burls.Windows/MainWindow.xaml.cs
@@ -4,15 +4,22 @@ using Burls.Windows.Services;
 using Burls.Windows.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using PInvoke;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using static PInvoke.User32;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,7 +42,9 @@ namespace Burls.Windows
             _shellPage = shellPage;
             _serviceProvider = serviceProvider;
 
-            this.Activated += MainWindow_Activated;
+            Activated += MainWindow_Activated;
+
+            SetWindowDetails(1180, 860);
         }
 
         private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
@@ -63,6 +72,28 @@ namespace Burls.Windows
             {
                 _shellPage.MainNavigation.SelectedItem = selectedItem;
             }
+        }
+
+        private void SetWindowDetails(int width, int height)
+        {
+            // Get window info
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var dpi = GetDpiForWindow(hwnd);
+            float scalingFactor = (float)dpi / 96;
+            width = (int)(width * scalingFactor);
+            height = (int)(height * scalingFactor);
+
+            // Get monitor info
+            var monitor = MonitorFromWindow(hwnd, MonitorOptions.MONITOR_DEFAULTTONEAREST);
+            GetMonitorInfo(monitor, out var info);
+            int monitor_width = info.Monitor.right - info.Monitor.left;
+            int monitor_height = info.Monitor.bottom - info.Monitor.top;
+
+            // Calc new position (Center screen)
+            Point pt = new Point((monitor_width - width) / 2, (monitor_height - height) / 2);
+
+            // Set new position
+            SetWindowPos(hwnd, SpecialWindowHandles.HWND_TOPMOST, (int)pt.X, (int)pt.Y, width, height, SetWindowPosFlags.SWP_SHOWWINDOW);
         }
     }
 }
